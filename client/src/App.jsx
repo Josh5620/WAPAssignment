ae0dc17f5921e4fd9658a94fbb4068ba42648a91
Josh5620 <josh.ong2005@gmail.com>
2025-08-20 23:43:50 +0800

    qwerwertartetre

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import HomePage from './pages/HomePage' 
-import RegisterPage from './pages/RegisterPage'
-import AdminLogin from './pages/AdminLogin'
-import AdminDashboard from './pages/AdminDashboard'
-import ChatBotPopup from './components/ChatBotPopup'
-import ExplainTest from './pages/ExplainTest'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import ChatBotPopup from "./components/ChatBotPopup";
+import ExplainTest from "./pages/ExplainTest";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/teacherLogin" element={<Login />} />
-        <Route path="/studentLogin" element={<Login />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/explainTest" element={<ExplainTest />} />
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </Router>
-    <ChatBotPopup></ChatBotPopup>
+      <Router>
+        <Routes>
+          <Route path="/teacherLogin" element={<Login />} />
+          <Route path="/studentLogin" element={<Login />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/explainTest" element={<ExplainTest />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </Router>
+      <ChatBotPopup></ChatBotPopup>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/ProjectAPI/obj/Debug/net9.0/ProjectAPI.AssemblyInfo.cs
+++ server/ProjectAPI/obj/Debug/net9.0/ProjectAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed35ced4255ef75ff04ea164e08219fed7d86827")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd4bd0e11094aba636d7010637bb02df28f7da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

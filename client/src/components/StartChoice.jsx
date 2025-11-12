fa4511cb55647db0b67911a416a821e30c99a88e
Joshua Ong <josh.ong2005@gmail.com>
2025-11-12 10:30:14 +0800

    Remove unused legacy components

--- client/src/components/StartChoice.jsx
+++ client/src/components/StartChoice.jsx
@@ -1,17 +1,1 @@
-import { useNavigate } from "react-router-dom";
-import React from "react";
-import "../styles/StartChoice.css";
-
-const StartChoice = ({ startLoc, text }) => {
-  const navigate = useNavigate();
-  const handleLogin = () => {
-    navigate(startLoc);
-  };
-
-  return (
-    <button className="login-choice-button" onClick={handleLogin}>
-      {text}
-    </button>
-  );
-};
-export default StartChoice;
+// removed unused code: StartChoice button component was no longer imported anywhere
